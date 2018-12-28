5bcac5eab9a851881ec9086778d4bc6480155ef9
Lopez <AndLopez@correo.local>
2018-12-28 14:59:20 -0300

    fin del ejercicio.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -10,14 +10,17 @@ import { Heroe } from 'src/app/interfaces/heroe.interface';
 export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
+  loading:boolean = true;
+
 
   constructor( private _heroes:HeroesService ) {
 
     this._heroes.getHeroes ().subscribe (response => {
+      console.log (response);
 
       this.heroes = response;
-      console.log (this.heroes);
-    })
+
+    }).add ( ()=> {this.loading = false;})
 
 
   }
